f44b5f4d2b248be07927bb7d801dd46b3fa2fea1
M_Uimina <m2a0s0h@gmail.ru>
2025-01-22 18:21:57 +0300

    flask

--- TimeWeb/db.py
+++ TimeWeb/db.py
@@ -1,12 +1,20 @@
 import sqlite3
-
 db_lp = sqlite3.connect('login_password.db')
 cursor_db = db_lp.cursor()
-sql_create = '''CREATE TABLE passwords(
-login TEXT PRIMARY KEY,
-password TEXT NOT NULL);'''
+
+sql_create = '''CREATE TABLE IF NOT EXISTS passwords (
+    login TEXT PRIMARY KEY,
+    password TEXT NOT NULL
+);'''
 
 cursor_db.execute(sql_create)
+cursor_db.execute('''ALTER TABLE passwords ADD COLUMN role TEXT''')
+
+sql_create2 = '''CREATE TABLE IF NOT EXISTS tags (
+    name TEXT PRIMARY KEY,
+    count TEXT NOT NULL
+);'''
+cursor_db.execute(sql_create2)
 db_lp.commit()
 
 cursor_db.close()
--- TimeWeb/main.py
+++ TimeWeb/main.py
@@ -1,7 +1,69 @@
-from flask import Flask, request, render_template
+import os
+
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 import sqlite3
 
 app = Flask(__name__)
+
+app.secret_key = os.urandom(24)
+def get_db_connection():
+    conn = sqlite3.connect('login_password.db')
+    # Возвращаем строки как "словари"
+    conn.row_factory = sqlite3.Row
+    return conn
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/users")
+def get_users():
+    conn = get_db_connection()
+    users = conn.execute('SELECT * FROM passwords').fetchall()
+    conn.close()
+    return render_template('users.html', users=users)
+@app.route('/create_user', methods=('GET', 'POST'))
+def create_user():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('password')
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO passwords (login, password) VALUES (?, ?)', (login, password))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('get_users'))
+
+    return render_template('create.html')
+
+@app.route('/create_tag', methods=('GET', 'POST'))
+def create_tag():
+    if request.method == 'POST':
+        tag = request.form.get('tag')
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO tags (name, count) VALUES (?, ?)', (tag, 0))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('get_tags'))
+
+    return render_template('create_tag.html')
+
+@app.route("/tags")
+def get_tags():
+    conn = get_db_connection()
+    tags = conn.execute('SELECT * FROM tags').fetchall()
+    conn.close()
+    return render_template('tags.html', tags=tags)
 
 @app.route('/authorization', methods=['GET', 'POST'])
 def form_authorization():
@@ -11,26 +73,41 @@ def form_authorization():
 
        db_lp = sqlite3.connect('login_password.db')
        cursor_db = db_lp.cursor()
-       cursor_db.execute(('''SELECT password FROM passwords
-                                               WHERE login = '{}';
-                                               ''').format(Login))
-       pas = cursor_db.fetchall()
 
+       cursor_db.execute(('SELECT password FROM passwords WHERE login = ?'), (Login,))
+       result = cursor_db.fetchone()
+
+
+       if not result:
+           return render_template("authorization.html", error_message='Пользователя с таким логином не существует!')
+       if result[0] != Password:
+           return render_template("authorization.html", error_message='Неверный пароль!')
+
+       cursor_db.execute('SELECT role FROM passwords WHERE login = ?', (Login,))
+       role = cursor_db.fetchone()[0]
+
+       session['username'] = Login
+       session['role'] = role
+
+       # Закрываем подключение к базе данных
        cursor_db.close()
-       try:
-           if pas[0][0] != Password:
-               return render_template('badauth.html')
-       except:
-           return render_template('badauth.html')
 
-       db_lp.close()
-       return render_template('successfulauth.html')
-
+       return redirect(url_for('home'))
    return render_template('authorization.html')
+
+@app.route('/logout')
+def logout():
+    # Удаляем имя пользователя из сессии (выход из аккаунта)
+    session.pop('username', None)
+    session.clear()  # Очистка всех данных сессии
+    return redirect(url_for('home'))
+
+@app.route('/account', methods=('GET', 'POST'))
+def account():
+    return render_template('account.html')
 
 @app.route('/registration', methods=['GET', 'POST'])
 def form_registration():
-
    if request.method == 'POST':
        Login = request.form.get('Login')
        Password = request.form.get('Password')
